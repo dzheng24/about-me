0ec6f4c769c59eb40d663b458e58fab0722bc824
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-08-07 18:47:37 -0700

    updated question 6 and 7

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var score = 0;
+
 var answers = [];
 
 var person = confirm('Confirm you are a real person');
@@ -23,7 +25,9 @@ if (answerSport.charAt(0)==='y'){
 if (answerSport.charAt(0)==='n'){
   console.log('Correct');
   alert ('You got it, skateboarding is my jam');
+  score++;
 }
+
 answers.push(answerSport);
 
 //Question about Food
@@ -32,6 +36,7 @@ answerFood = answerFood.toLowerCase();
 if (answerFood.charAt(0)==='y'){
   console.log ('Correct');
   alert ('Yes! I love Mexican food!');
+  score++;
 }
 
 if (answerFood.charAt(0)==='n'){
@@ -46,6 +51,7 @@ answerMusic = answerMusic.toLowerCase();
 if (answerMusic.charAt(0)==='y'){
   console.log ('Correct');
   alert ('Drake is my guy!');
+  score++;
 }
 
 if (answerMusic.charAt(0)==='n'){
@@ -65,6 +71,7 @@ if (answerPlace.charAt(0)==='y'){
 if (answerPlace.charAt(0)==='n'){
   console.log('Correct');
   alert ('You got it, I\'d love to go there sometime');
+  score++;
 }
 answers.push(answerPlace);
 
@@ -79,6 +86,54 @@ if (answerCandy.charAt(0)==='y'){
 if (answerCandy.charAt(0)==='n'){
   console.log('Correct');
   alert ('Right on, my favorite is sour candies, like sour patch kids');
+  score++;
 }
 answers.push(answerCandy);
+
+//Question 6 "number of my shoes"
+
+var guessesTaken = 0;
+while (guessesTaken<4){
+  guessesTaken = guessesTaken + 1;
+  var number = 5;
+  var guess = prompt('Guess how many pairs of shoes I have!');
+  guess = Number(guess);
+  if (guess < number){
+    alert('Your guess is too low');
+  }
+  if (guess > number){
+    alert('Your guess is too high');
+  }
+  if (guess === number){
+    score++;
+    alert('Wow! You got it!');
+    console.log('Correct');
+    break;
+  }
+}
+if (guess !== number){
+  console.log('False');
+  alert('Sorry you didn\'t get it, I have 5 pairs of shoes.');
+}
+
+//Question 7 "states I've lived in before Washington"
+
+for (var t=1; t<6; t++){
+  var states = ['michigan','california','florida'];
+  var guessSeven = prompt ('Guess what state I have lived in before Washington.');
+
+  for (var a=0; a<states.length;a++){
+    if (guessSeven.toLowerCase() === states[a]){
+      console.log('Correct');
+      alert('Yes! I have lived in michigan, california, and florida.');
+      score++;
+      t=7;
+      break;
+    }
+  }
+}
+
+alert('You got a '+ score + ' out of 7 on this!');
+
+
 
