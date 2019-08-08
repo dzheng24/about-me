5a32294306a7fb8e84516afc0b837648e526082b
Scuba-Chris <chrisceder@hotmail.com>
2019-08-08 16:20:10 -0700

    added functions and while loop to question 7

--- js/app.js
+++ js/app.js
@@ -1,137 +1,155 @@
-'use strict';
+// 'use strict';
 
 var score = 0;
 
-var answers = [];
 
-var person = confirm('Confirm you are a real person');
+// var person = confirm('Confirm you are a real person');
+// console.log (' user Is Real');
 
-answers.push(person);
-console.log (person);
 
-if (person) {
-  alert('Thank you, have fun playing the game');
-}
-else alert ('Enjoy the page, robot');
+// if (person) {
+//   alert('Thank you, have fun playing the game');
+// }
+// else alert ('Enjoy the page, robot');
 
-//Question about Sport
-var answerSport = prompt('Soccer is my favorite sport. Yes or no?');
-answerSport = answerSport.toLowerCase();
-if (answerSport.charAt(0)==='y'){
-  console.log ('False');
-  alert ('Nope, my favorite sport is skateboarding!');
-}
+// //Question about Sport
+// function questionAboutSport() {
+//   var answerSport = prompt('Soccer is my favorite sport. Yes or no?');
+//   answerSport = answerSport.toLowerCase();
+//   if (answerSport.charAt(0)==='y'){
+//     console.log ('question about sport: false');
+//     alert ('Nope, my favorite sport is skateboarding!');
+//   }
 
-if (answerSport.charAt(0)==='n'){
-  console.log('Correct');
-  alert ('You got it, skateboarding is my jam');
-  score++;
-}
+//   if (answerSport.charAt(0)==='n'){
+//     console.log('question about sport: correct');
+//     alert ('You got it, skateboarding is my jam');
+//     score++;
+//   }
+// }
+// questionAboutSport();
 
-answers.push(answerSport);
+// // Question about Food
+// function questionAboutFood() { 
+//   var answerFood = prompt('I can eat Mexican food every day. Yes or no');
+//   answerFood = answerFood.toLowerCase();
+//   if (answerFood.charAt(0)==='y'){
+//     console.log ('question about food: correct');
+//     alert ('Yes! I love Mexican food!');
+//     score++;
+//   }
 
-//Question about Food
-var answerFood = prompt('I can eat Mexican food every day. Yes or no');
-answerFood = answerFood.toLowerCase();
-if (answerFood.charAt(0)==='y'){
-  console.log ('Correct');
-  alert ('Yes! I love Mexican food!');
-  score++;
-}
+//   if (answerFood.charAt(0)==='n'){
+//     console.log('question about food: false');
+//     alert ('Nah, I actually can.');
+//   }
+// }
+// questionAboutFood();
 
-if (answerFood.charAt(0)==='n'){
-  console.log('False');
-  alert ('Nah, I actually can.');
-}
-answers.push(answerFood);
+// // Question about Music
+// function questionAboutMusic(){
+//   var answerMusic = prompt('Drake is my favorite artist. Yes or no?');
+//   answerMusic = answerMusic.toLowerCase();
+//   if (answerMusic.charAt(0)==='y'){
+//     console.log ('question about music: correct');
+//     alert ('Drake is my guy!');
+//     score++;
+//   }
 
-//Question about Music
-var answerMusic = prompt('Drake is my favorite artist. Yes or no?');
-answerMusic = answerMusic.toLowerCase();
-if (answerMusic.charAt(0)==='y'){
-  console.log ('Correct');
-  alert ('Drake is my guy!');
-  score++;
-}
+//   if (answerMusic.charAt(0)==='n'){
+//     console.log('question about music: false');
+//     alert ('Who doesn\'t like Drake?');
+//   }
+// }
+// questionAboutMusic();
 
-if (answerMusic.charAt(0)==='n'){
-  console.log('Falset');
-  alert ('Who doesn\'t like Drake?');
-}
-answers.push(answerMusic);
+// // Question about Place
+// function questionPlace() {
+//   var answerPlace = prompt('I have been to Brazil before. Yes or no?');
+//   answerPlace = answerPlace.toLowerCase();
+//   if (answerPlace.charAt(0)==='y'){
+//     console.log ('question about place: false');
+//     alert ('Nope, I\'ve always wanted to go there!');
+//   }
 
-//Question about Place
-var answerPlace = prompt('I have been to Brazil before. Yes or no?');
-answerPlace = answerPlace.toLowerCase();
-if (answerPlace.charAt(0)==='y'){
-  console.log ('False');
-  alert ('Nope, I\'ve always wanted to go there!');
-}
+//   if (answerPlace.charAt(0)==='n'){
+//     console.log('question about place: correct');
+//     alert ('You got it, I\'d love to go there sometime');
+//     score++;
+//   }
+// }
+// questionPlace();
 
-if (answerPlace.charAt(0)==='n'){
-  console.log('Correct');
-  alert ('You got it, I\'d love to go there sometime');
-  score++;
-}
-answers.push(answerPlace);
+// //Question about Candy
+// function questionCandy() { 
+//   var answerCandy = prompt('My favorite candy is chocolate. Yes or no?');
+//   answerCandy = answerCandy.toLowerCase();
+//   if (answerCandy.charAt(0)==='y'){
+//     console.log ('question about candy: false');
+//     alert ('Nope, my favorite candy is anything sour! Like Sour Patch Kids');
+//   }
 
-//Question about Candy
-var answerCandy = prompt('My favorite candy is chocolate. Yes or no?');
-answerCandy = answerCandy.toLowerCase();
-if (answerCandy.charAt(0)==='y'){
-  console.log ('False');
-  alert ('Nope, my favorite candy is anything sour! Like Sour Patch Kids');
-}
-
-if (answerCandy.charAt(0)==='n'){
-  console.log('Correct');
-  alert ('Right on, my favorite is sour candies, like sour patch kids');
-  score++;
-}
-answers.push(answerCandy);
+//   if (answerCandy.charAt(0)==='n'){
+//     console.log('question about candy: correct');
+//     alert ('Right on, my favorite is sour candies, like sour patch kids');
+//     score++;
+//   }
+// }
+// questionCandy();
 
 //Question 6 "number of my shoes"
 
-var guessesTaken = 0;
-while (guessesTaken<4){
-  guessesTaken = guessesTaken + 1;
-  var number = 5;
-  var guess = prompt('Guess how many pairs of shoes I have!');
-  guess = Number(guess);
-  if (guess < number){
-    alert('Your guess is too low');
-  }
-  if (guess > number){
-    alert('Your guess is too high');
-  }
-  if (guess === number){
-    score++;
-    alert('Wow! You got it!');
-    console.log('Correct');
-    break;
-  }
-}
-if (guess !== number){
-  console.log('False');
-  alert('Sorry you didn\'t get it, I have 5 pairs of shoes.');
-}
+// function shoes() {
+//   var guessesTaken = 0;
+//   while (guessesTaken<4){
+//     guessesTaken = guessesTaken + 1;
+//     var number = 5;
+//     var guess = prompt('Guess how many pairs of shoes I have!');
+//     guess = Number(guess);
+//     if (guess < number){
+//       alert('Your guess is too low');
+//     }
+//     if (guess > number){
+//       alert('Your guess is too high');
+//     }
+//     if (guess === number){
+//       score++;
+//       alert('Wow! You got it!');
+//       console.log('question about shoes: correct');
+//       break;
+//     }
+//   }
+//   if (guess !== number){
+//     console.log('questions about shoes: false');
+//     alert('Sorry you didn\'t get it, I have 5 pairs of shoes.');
+//   }
+// }
+// shoes();
 
 //Question 7 "states I've lived in before Washington"
+var states = ['michigan','california','florida'];
+var statesGuess = false;
+var statesTries = 1;
 
-for (var t=1; t<6; t++){
-  var states = ['michigan','california','florida'];
-  var guessSeven = prompt ('Guess what state I have lived in before Washington.');
-
-  for (var a=0; a<states.length;a++){
-    if (guessSeven.toLowerCase() === states[a]){
-      console.log('Correct');
-      alert('Yes! I have lived in michigan, california, and florida.');
-      score++;
-      t=7;
-      break;
+function questionsStates () {
+  while( statesTries <= 7 && statesGuess === false ) {
+    var guessSeven = prompt ('Guess what state I have lived in before Washington.');
+    for ( var i=0 ; i < states.length; i++) {
+      if (guessSeven.toLowerCase() === states[i]) {
+        alert('yes i have lived in Michigan, Califorina, and Florida');
+        score++;
+        console.log('question 7 correct');
+        statesGuess = true;
+      }
     }
+    if (! statesGuess){
+      alert('sorry you got it wrong');
+      console.log('question 7: wrong');
+    }
+    statesTries++;
   }
 }
+questionsStates();
 
 alert('You got a '+ score + ' out of 7 on this!');
 
